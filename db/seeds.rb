7afd611d815446c10216333de7be0b065ee46b29
Sheila Adesina <sheilaadesina@192-168-1-162.tpgi.com.au>
2015-10-07 20:18:43 +1100

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.create(user_type: 'doctor', email: 'doctor1@test.com', password: '12345678')
+DoctorProfile.create(user_id: user1.id, title: 'Dr', first_name: 'Johnny', last_name: 'Smith')
+
+
+user2 = User.create(user_type: 'doctor', email: 'doctor2@test.com', password: '12345678')
+DoctorProfile.create(user_id: user2.id,title: 'Dr', first_name: 'Rafael', last_name: 'arruda')
+
+ user3 = User.create(user_type: 'doctor', email: 'doctor3@test.com', password: '12345678')
+DoctorProfile.create(user_id: user3.id,title: 'Dr', first_name: 'Guy', last_name: 'Gilead')
+
+user4 = User.create(user_type: 'doctor', email: 'doctor4@test.com', password: '12345678')
+DoctorProfile.create(user_id: user4.id, title: 'Dr', first_name: 'Rachel', last_name: 'costa')
